5441de53205c58ee0e065582c388f4c5bd4a870c
eugeneb-1984 <eugeneb1984@gmail.com>
2021-06-27 13:45:59 +0300

    Adding task 4-2

---
+++ GBHomeWork4/Task4_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите несколько чисел через пробел:");
+            string stringOfNumbers = Console.ReadLine();
+            Console.WriteLine($"Посчитанная сумма: {GetSumByNumbers(stringOfNumbers)}");
+            Console.WriteLine("Нажмите любую клавишу чтобы закрыть программу");
+            Console.ReadKey();
+
+            static int GetSumByNumbers(string stringOfNumbers)
+            {
+                int sum = 0;
+                foreach (var number in stringOfNumbers.Split(' '))
+                {
+                    int num;
+                    if (int.TryParse(number, out num))
+                    {
+                        sum +=num; 
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод. Убедитесь, что все введённые значения являются числами и введены именно через пробел.");
+                        sum = 0;
+                        break;
+                    }
+                }
+                return sum;
+            }
+        }
+    }
+}
