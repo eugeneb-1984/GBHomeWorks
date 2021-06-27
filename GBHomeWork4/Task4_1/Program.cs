beb754ee8856bae17fe034d924338226d394b625
eugeneb-1984 <eugeneb1984@gmail.com>
2021-06-27 12:17:58 +0300

    Adding task 4-1

---
+++ GBHomeWork4/Task4_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ivanFirstName = "Иван";
+            string ivanPatronymic = "Иванович";
+            string ivanLastName = "Иванов";
+
+
+            string petrFirstName = "Пётр";
+            string petrPatronymic = "Петрович";
+            string petrLastName = "Петров";
+
+            string andreyFirstName = "Андрей";
+            string andreyPatronymic = "Андреевич";
+            string andreyLastName = "Андреев";
+
+
+            Console.WriteLine("Вывод ФИО полностью:");
+            GetFullName(ivanFirstName, ivanLastName, ivanPatronymic);
+            GetFullName(petrFirstName, petrLastName, petrPatronymic);
+            GetFullName(andreyFirstName, andreyLastName, andreyPatronymic);
+         
+
+            static void GetFullName(string firstName, string lastName, string patronymic)
+
+            {
+                Console.WriteLine($"{firstName} {patronymic} {lastName}");
+            }
+        }
+    }
+}
