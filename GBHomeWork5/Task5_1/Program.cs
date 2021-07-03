ddab17e35339db8215d1b1e5a37a85e971620f91
eugeneb-1984 <eugeneb1984@gmail.com>
2021-07-03 12:41:58 +0300

    Adding task 5-1

---
+++ GBHomeWork5/Task5_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Task5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любой текст:");
+            var input = Console.ReadLine();
+            string filename = "input.txt";
+            File.AppendAllText(filename, input);
+            Console.WriteLine($"Спасибо, текст записан в файл {filename}");
+            Console.ReadKey();
+        }
+    }
+}
