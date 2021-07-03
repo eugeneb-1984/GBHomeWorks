e2b22b56420e73a7fd7a638691f0a5be03d57106
eugeneb-1984 <eugeneb1984@gmail.com>
2021-07-03 13:06:26 +0300

    Adding task 5-2

---
+++ GBHomeWork5/Task5_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Task5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CurrentTime = $"{DateTime.Now:t}\n";
+            File.AppendAllText("startup.txt", CurrentTime);
+        }
+    }
+}
